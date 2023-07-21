41be348f79051455d99fec619b478c3ce3057f22
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-07-21 20:59:31 +0100

    5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,7 @@
 # Write a program that prints numbers from 0 to 99
-# Implemented code for printing numbers from 0 to 99
 for i in range(100):
-    print("{:02d}".format(i), end='')
-    if i < 99:
-        print(", ", end='')
+    end = "" if i == 99 else ", "
+    print("{:02d}{}".format(i, end), end='')
 
 print("\n")
 
