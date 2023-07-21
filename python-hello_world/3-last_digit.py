e40e0dd0bf94d42a822e11df15c77d015f091b6f
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-07-21 21:04:00 +0100

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,10 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE/
-last = str(number)[-1]
-if int(last) > 5:
-    print("Last digit of", number, "is", last, "and is greater than 5")
-elif int(last) < 6 and int(last) != 0:
-    print("Last digit of", number, "is", last, "and is less than 6 and not 0")
+# Compute the last digit of the number (using absolute value to handle negative numbers)
+last_digit = abs(number) % 10
+
+# Determine the corresponding text based on the last digit
+if last_digit > 5:
+    text = "and is greater than 5"
+elif last_digit == 0:
+    text = "and is 0"
 else:
-    print("Last digit of", number, "is", last, "and is 0")
+    text = "and is less than 6 and not 0"
+
+# Output the full sentence
+print("Last digit of {} is {} {}".format(number, last_digit, text))
+
