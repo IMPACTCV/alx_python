c5151a21470b052db617223d2f4d80c1beb29f5f
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-07-21 21:07:21 +0100

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE/
-# get the last digit of the number
+# Calculate the last digit of the number
 last_digit = abs(number) % 10
 
-# get the polarity of the number 
+# Determine the polarity of the number
 polarity = -1 if number < 0 else 1
 
-# determine the message
+# Determine the message based on the value of the last digit
 if last_digit > 5:
     message = "and is greater than 5"
 elif last_digit == 0:
@@ -16,6 +16,6 @@ elif last_digit == 0:
 else:
     message = "and is less than 6 and not 0"
 
-# print the complete message
-print(f"Last digit of {polarity*number:d} is {polarity*last_digit} {message}")
+# Print the complete message
+print(f"Last digit of {polarity * number} is {polarity * last_digit} {message}")
 
