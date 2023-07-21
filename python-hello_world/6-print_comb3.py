d1198c8e4104d75bb09386d54652cbae3d706eee
Yopa Nelly <yopatatchou@gmail.com>
2023-07-21 15:41:32 +0100

    Added files

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,6 @@
+# . Inventing is a combination of brains and materials. The more brains you use, the less material you need
+for i in range (10):
+    for j in range(i + 1, 10):
+        print("{}{}, ".format(i, j), end='', )
+
+print("\n")
