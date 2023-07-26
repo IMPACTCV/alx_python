3002945aa4cfb57f71f3d6ffcf54bc0fe9b5e0f7
Yopa Nelly <yopatatchou@gmail.com>
2023-07-26 21:25:07 +0100

    add

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,5 +1,12 @@
 def safe_print_division(a, b):
-    return "{:d} / {:d} = {}".format(a, b, a/b)
+    try:
+        return "{:d} / {:d} = {}".format(a, b, a/b)
+    except ZeroDivisionError:
+        return "Error: Can't divide by zero"
+    except ValueError:
+        return "Error: Non-numeric input"
+
+    
 
 
 a = 12
