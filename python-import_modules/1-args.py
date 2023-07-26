24fa35d71e8a2412b9999c89d84ff95c2b95d1a1
Yopa Nelly <yopatatchou@gmail.com>
2023-07-26 21:00:23 +0100

    add

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,39 +1,41 @@
 #  How to make a script dynamic!
-import sys
-
-# Get the number of arguments
-num_args = len(sys.argv) - 1
-arg = sys.argv
-def argument(arg):
-    count = 1
-    if len(arg) == 1:
-        print("{} argument:".format(num_args))
-        for i in arg:
-            print("{}: {}".format(count, i))
-    elif len(arg) == 0:
-        print(".")
-    else:
-        print("{} arguments:".format(num_args))
-        for i in arg:
-        
-            print("{}: {}".format(count, i))
-            count +=1
-
-
-if __name__ == "__main__":
-    
-
-
-
-# #!/usr/bin/env python3
 # import sys
 
 # # Get the number of arguments
 # num_args = len(sys.argv) - 1
+# arg = sys.argv
+# def argument(arg):
+#     count = 1
+#     if len(arg) == 1:
+#         print("{} argument:".format(num_args))
+#         for i in arg:
+#             print("{}: {}".format(count, i))
+#     elif len(arg) == 0:
+#         print(".")
+#     else:
+#         print("{} arguments:".format(num_args))
+#         for i in arg:
+        
+#             print("{}: {}".format(count, i))
+#             count +=1
 
-# # Print the number of arguments and "argument(s)" with a newline
-# print("{} argument{}.".format(num_args, "s" if num_args != 1 else ""))
+
+# if __name__ == "__main__":
     
-# # Print each argument on a separate line
-# for i, arg in enumerate(sys.argv[1:], start=1):
-#     print("{}: {}".format(i, arg))
+import sys
+
+if __name__ == "__main__":
+    # Get the number of arguments (excluding the script name)
+    num_args = len(sys.argv) - 1
+
+    # Print the number of arguments and "argument(s)", followed by a colon or period depending on whether there are arguments
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
+
+        # Print each argument with its position
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
+
+
