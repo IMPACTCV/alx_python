bc80fae821b6a7db87c3a30b30d8d1ec72c14cd2
Yopa Nelly <yopatatchou@gmail.com>
2023-07-26 23:46:38 +0100

    add

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -14,6 +14,5 @@ def multiple_returns(sentence):
         return len(sentence), sentence[0]
 
 
-sentence = "At Holberton school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
