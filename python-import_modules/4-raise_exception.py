150d2d986c1b32a2e6d1a354f33fd2e073637bf1
Yopa Nelly <yopatatchou@gmail.com>
2023-07-26 22:09:05 +0100

    Add

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,5 +1,10 @@
 def raise_exception():
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception has been raised")
+    my_var = 123
+    my_var += "abc"  # This raises a TypeError
+
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
+
