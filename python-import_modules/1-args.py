cb554f907043acba550bfc9531172fdff5f82fe3
Yopa Nelly <yopatatchou@gmail.com>
2023-07-26 20:29:33 +0100

    Added

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,34 +1,39 @@
 #  How to make a script dynamic!
-# def argument(argv=[]):
-#     count = 1
-#     if len(argv) == 1:
-#         print("{} argument:".format(len(argv)))
-#         for i in argv:
-#             print("{}: {}".format(count, i))
-#     elif len(argv) == 0:
-#         print(".")
-#     else:
-#         print("{} arguments:".format(len(argv)))
-#         for i in argv:
-        
-#             print("{}: {}".format(count, i))
-#             count +=1
-
-
-# if __name__ == "__main__":
-
-#     print(argument([]))
-
-
-#!/usr/bin/env python3
 import sys
 
 # Get the number of arguments
 num_args = len(sys.argv) - 1
+arg = sys.argv
+def argument(arg):
+    count = 1
+    if len(argv) == 1:
+        print("{} argument:".format(num_args))
+        for i in arg:
+            print("{}: {}".format(count, i))
+    elif len(arg) == 0:
+        print(".")
+    else:
+        print("{} arguments:".format(num_args))
+        for i in arg:
+        
+            print("{}: {}".format(count, i))
+            count +=1
 
-# Print the number of arguments and "argument(s)" with a newline
-print("{} argument{}.".format(num_args, "s" if num_args != 1 else ""))
+
+if __name__ == "__main__":
+    print("s")
+
+
+
+# #!/usr/bin/env python3
+# import sys
+
+# # Get the number of arguments
+# num_args = len(sys.argv) - 1
+
+# # Print the number of arguments and "argument(s)" with a newline
+# print("{} argument{}.".format(num_args, "s" if num_args != 1 else ""))
     
-# Print each argument on a separate line
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+# # Print each argument on a separate line
+# for i, arg in enumerate(sys.argv[1:], start=1):
+#     print("{}: {}".format(i, arg))
