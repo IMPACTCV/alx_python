7685449e69f7a994437c800f28c53c5a68030bb7
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-07-26 22:04:01 +0100

    4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,5 @@
 def raise_exception():
     try:
-        x = 1 + "string"
-    except TypeError:
-        print("Exception raised")
-        raise
-
+    raise_exception()
+except TypeError as te:
+    print("Exception has been raised")
