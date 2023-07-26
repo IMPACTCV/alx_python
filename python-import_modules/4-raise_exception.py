e3946e7d6aad434a593556143b6c1f5550afe05c
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-07-26 22:01:59 +0100

    4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,11 +1,7 @@
 def raise_exception():
-    x = "string"  # This is a string object
-    if type(x) != int:
-        raise TypeError  # Raise TypeError if x is not of type int
-
-
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    try:
+        x = 1 + "string"
+    except TypeError:
+        print("Exception raised")
+        raise
 
