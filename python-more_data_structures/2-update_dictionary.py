9cb4e3763929ad5a0a191f752501c8802ba5d6fc
Yopa Nelly <yopatatchou@gmail.com>
2023-07-27 23:39:40 +0100

    Some changes

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,8 +1,3 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-
-
-a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print(new_dict)
