4e07dd9922b1261321a6869b0c98549d04f596e9
Yopa Nelly <yopatatchou@gmail.com>
2023-08-01 12:08:32 +0100

    Add

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,5 @@
 """"
 class: defined class call square 
-
 """
 class Square:
     """
@@ -19,16 +18,3 @@ class Square:
             raise TypeError("size must be an integer")
         elif size<0:
             raise ValueError("size must be >= 0")
-
-
-
-#     def set_size(self, size=0):
-#         self.__size = size
-#         if type(size) != int:
-#             raise TypeError("size must be an integer")
-#         elif size<0:
-#             raise ValueError("size must be >= 0")
-#     def get_size(self):
-#         return self.__size
-# h = 54
-# print(type(4545))
