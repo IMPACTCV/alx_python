9e9607ad0f950c7cbace2f4b5035213f39db7198
Yopa Nelly <yopatatchou@gmail.com>
2023-08-01 14:26:58 +0100

    added files

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -52,13 +52,3 @@ class Square:
                 print("#" * self.__size)
             else:
                 print(" ")
-
-my_square = Square(3)
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
