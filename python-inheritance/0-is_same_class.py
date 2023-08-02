9fbdd2e8a9f2d6dff7b22a8e96a3949c7c8013c8
Yopa Nelly <yopatatchou@gmail.com>
2023-08-02 15:32:19 +0100

    Added documentation

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -5,7 +5,11 @@ is an instance of a class
 """
 define the function
 """
-def is_ame_class(obj, a_class):
+"""
+function 
+"""
+def is_ame_class(obj, a_class): # the function
+
     """
     return the T or F
     if the object is an instance of that class
