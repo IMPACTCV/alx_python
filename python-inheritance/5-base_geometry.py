d9b7dfc82e87f1671fc4c4abdc7ecd76ac5c4565
Yopa Nelly <yopatatchou@gmail.com>
2023-08-06 10:23:35 +0100

    add

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,4 +1,4 @@
-"""Integer validator
+"""Improve Geometry
 
 --------
 
@@ -21,23 +21,27 @@ class BaseGeometry:
     Raise:
     Exception with meassage
     """
+    def __dir__(self):
+        """
+        define the function:
+
+    parameters-->
+    self -> no parameters
+        """
+        attributes = super().__dir__()
+        new_attribute_list = [x for x in attributes if x != "__init_subclass__"]
+        return new_attribute_list
+    
     def area(self):
+        """
+        Function to raise exception
+        """
         raise Exception("area() is not implemented")
     
-    """
-    Public:
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
 
-    -------
-
-    def integer_validator(self, name, value): --> validates value
-    """
-
-    def integer_validator(self, name: str , value):
-        self.name = name
-        if type(self.value) != int:
-            raise TypeError("<name> must be an integer")
-        elif self.value <= 0:
-            raise ValueError("<name> must be greater than 0")
-        else:
-            self.value = value
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
 
