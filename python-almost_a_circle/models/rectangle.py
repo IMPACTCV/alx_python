f9d4398e463eae36e059244a197032107d9f757f
Yopa Nelly <yopatatchou@gmail.com>
2023-08-10 10:37:05 +0100

    add

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -169,80 +169,80 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
     
-    def _validate_width(self):
-        """Validate the width attribute."""
-        if not isinstance(self.__width, int):
-            raise TypeError("width must be an integer")
-        if self.__width <= 0:
-            raise ValueError("width must be > 0")
+    # def _validate_width(self):
+    #     """Validate the width attribute."""
+    #     if not isinstance(self.__width, int):
+    #         raise TypeError("width must be an integer")
+    #     if self.__width <= 0:
+    #         raise ValueError("width must be > 0")
 
-    def _validate_height(self):
-        """Validate the height attribute."""
-        if not isinstance(self.__height, int):
-            raise TypeError("height must be an integer")
-        if self.__height <= 0:
-            raise ValueError("height must be > 0")
+    # def _validate_height(self):
+    #     """Validate the height attribute."""
+    #     if not isinstance(self.__height, int):
+    #         raise TypeError("height must be an integer")
+    #     if self.__height <= 0:
+    #         raise ValueError("height must be > 0")
 
-    def _validate_x(self):
-        """Validate the x attribute."""
-        if not isinstance(self.__x, int):
-            raise TypeError("x must be an integer")
-        if self.__x < 0:
-            raise ValueError("x must be >= 0")
+    # def _validate_x(self):
+    #     """Validate the x attribute."""
+    #     if not isinstance(self.__x, int):
+    #         raise TypeError("x must be an integer")
+    #     if self.__x < 0:
+    #         raise ValueError("x must be >= 0")
 
-    def _validate_y(self):
-        """Validate the y attribute."""
-        if not isinstance(self.__y, int):
-            raise TypeError("y must be an integer")
-        if self.__y < 0:
-            raise ValueError("y must be >= 0")
+    # def _validate_y(self):
+    #     """Validate the y attribute."""
+    #     if not isinstance(self.__y, int):
+    #         raise TypeError("y must be an integer")
+    #     if self.__y < 0:
+    #         raise ValueError("y must be >= 0")
         
-    def area(self):
-        """
-        Calculate the area of a rectangle.
+    # def area(self):
+    #     """
+    #     Calculate the area of a rectangle.
 
-        Returns:
-            int: The calculated area of the rectangle.
-        """
-        return self.__width * self.__height
+    #     Returns:
+    #         int: The calculated area of the rectangle.
+    #     """
+    #     return self.__width * self.__height
 
-    def display(self):
-        """
-        Display the rectangle using '#'.
+    # def display(self):
+    #     """
+    #     Display the rectangle using '#'.
 
-        Returns:
-            None.
-        """
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+    #     Returns:
+    #         None.
+    #     """
+    #     for _ in range(self.__y):
+    #         print()
+    #     for _ in range(self.__height):
+    #         print(" " * self.__x + "#" * self.__width)
     
-    def __str__(self):
-        """
-        Overrides __str__
+    # def __str__(self):
+    #     """
+    #     Overrides __str__
 
-        Returns:
-            formatted string of rectange attributes.
-        """
-        return("[Rectangle] ({}) {}/{} - {}/{}").format(self.id, self.__x, self.__y, self.__width, self.__height)
+    #     Returns:
+    #         formatted string of rectange attributes.
+    #     """
+    #     return("[Rectangle] ({}) {}/{} - {}/{}").format(self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args, **kwargs):
-        """
-        Update attributes of the Rectangle instance
-        """
-        if args:
-            num_args = len(args)
-            if num_args >= 1:
-                self.id = args[0]
-            if num_args >= 2:
-                self.__width = args[1]
-            if num_args >= 3:
-                self.__height = args[2]
-            if num_args >= 4:
-                self.__x = args[3]
-            if num_args >= 5:
-                self.__y = args[4]
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    # def update(self, *args, **kwargs):
+    #     """
+    #     Update attributes of the Rectangle instance
+    #     """
+    #     if args:
+    #         num_args = len(args)
+    #         if num_args >= 1:
+    #             self.id = args[0]
+    #         if num_args >= 2:
+    #             self.__width = args[1]
+    #         if num_args >= 3:
+    #             self.__height = args[2]
+    #         if num_args >= 4:
+    #             self.__x = args[3]
+    #         if num_args >= 5:
+    #             self.__y = args[4]
+    #     else:
+    #         for key, value in kwargs.items():
+    #             setattr(self, key, value)
