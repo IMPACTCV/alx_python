75112820c9435ae60b8d130a67bdb632bf6b11b0
Yopa Nelly <yopatatchou@gmail.com>
2023-08-10 16:25:48 +0100

    added

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,45 @@
+"""this script defines a clas square"""
+
+from models.rectangle import Rectangle
+
+class Sqare(Rectangle):
+    """
+    class defined
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Iniciatise
+
+        Args"
+        size-> size of the square
+        x: The x-coordinate of the square.
+        y: The y-coordinate of the square.
+            
+            id: The ID of the square.
+        
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: The formatted string containing the square information.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+    
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        """
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        """
+        self.width = self.height = value
