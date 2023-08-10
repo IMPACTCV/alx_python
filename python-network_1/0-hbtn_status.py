6cb49f9243b9eff41732431a1bf1a99a41695a51
Yopa Nelly <yopatatchou@gmail.com>
2023-08-10 23:02:02 +0100

    added files

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,3 +4,7 @@
 import requests
 
 response = requests.get("https://alu-intranet.hbtn.io/status")
+body = response.text
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
